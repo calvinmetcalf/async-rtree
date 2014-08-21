f604ff03dca573cd96a77381c2825f38dc2cb4ed
Calvin Metcalf <cmetcalf@appgeo.com>
2014-08-21 16:26:24 -0400

    bug with query streams

--- test.js
+++ test.js
@@ -3,6 +3,16 @@ var LT = require('./lib/index');
 var Promise = require('bluebird');
 var test = require('tape');
 var fs = require('fs');
+function promiseQuery(db, bbox) {
+  return new Promise(function (yes, no) {
+    var out = [];
+    db.query(bbox).on('data', function (d) {
+      out.push(d);
+    }).on('error', no).on('end', function () {
+      yes(out);
+    });
+  });
+}
 test('large query', function (t) {
   t.plan(1);
   var l = new LT();
@@ -40,6 +50,28 @@ test('small query', function (t) {
     return Promise.all(out);
   }).then(function (a) {
     return l.query([ [ 31, 30 ], [ 50, 60 ] ], true);
+  }).then(function(a){
+    t.equals(a.length, 2);
+  }).catch(function (e) {
+    t.notOk(e);
+  });
+
+});
+test('small query stream', function (t) {
+  t.plan(1);
+  var l = new LT();
+  var i = 100;
+  var promise = l.insert('lala', [[31,30],[40,40]]).then(function (a){
+    return l.insert('fafa',[[32,40],[50,60]]);
+  });
+  var out = [];
+  while (i--) {
+      out.push(l.insert('afa'+i,[[20-i,20-i],[40-i,20+i]]));
+  }
+  promise.then(function () {
+    return Promise.all(out);
+  }).then(function (a) {
+    return promiseQuery(l,[ [ 31, 30 ], [ 50, 60 ] ]);
   }).then(function(a){
     t.equals(a.length, 2);
   }).catch(function (e) {
