3d7cc0a4cec036958c0e4c4795cc5ff4f3400cbb
Calvin Metcalf <cmetcalf@appgeo.com>
2014-10-30 09:46:44 -0400

    append with the same id

--- test.js
+++ test.js
@@ -113,6 +113,39 @@ test('remove', function (t) {
   });
 
 });
+test('append', function (t) {
+  t.plan(4);
+  var l = new LT();
+  var i = 100;
+  var promise = l.append('lala', [[31,30],[40,40]]).then(function (a){
+    return l.append('lala',[[32,40],[50,60]]);
+  });
+  var out = [];
+  while (i--) {
+      out.push(l.append('afa',[[20-i,20-i],[40-i,20+i]]));
+  }
+  promise.then(function () {
+    return Promise.all(out);
+  }).then(function (a) {
+    return l.query([ [ 31, 30 ], [ 50, 60 ] ], true);
+  }).then(function(a){
+    t.equals(a.length, 2);
+    return l.query([ [ -10000, -10000 ], [ 10000, 10000 ] ], true);
+  }).then(function (a){
+    t.equals(a.length, 102);
+    return l.insert('lala', [[31,30],[40,40]]);
+  }).then(function (a) {
+    return l.query([ [ 31, 30 ], [ 50, 60 ] ], true);
+  }).then(function(a){
+    t.equals(a.length, 1);
+    return l.query([ [ -10000, -10000 ], [ 10000, 10000 ] ], true);
+  }).then(function (a){
+    t.equals(a.length, 101);
+  }).catch(function (e) {
+    t.notOk(e, e.stack);
+  });
+
+});
 test('remove no bbox', function (t) {
   t.plan(4);
   var l = new LT();
